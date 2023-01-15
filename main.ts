10781790009dda610a7ee58dea081dad1f7eee13
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-15 18:31:04 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
 let distancia = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distancia > 10) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    basic.pause(100)
 })
