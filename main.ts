ea6c8fe9c83098340403941c391fbce2d22bb882
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-15 19:04:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let dirección = 0
 let distancia = 0
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
@@ -6,13 +7,23 @@ basic.forever(function () {
         strip.showRainbow(1, 360)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-        strip.show()
-        basic.pause(200)
+        dirección = randint(0, 1)
+        if (dirección == 0) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            strip.show()
+            basic.pause(200)
+        } else {
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            strip.show()
+            basic.pause(200)
+        }
     }
     basic.pause(100)
 })
